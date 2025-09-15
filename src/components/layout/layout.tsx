1fb4f6d9f755904bb6d1503eb2587890f1544e41
M0HamedAhmed0 <mohamedahmed1454285@gmail.com>
2025-09-16 02:30:17 +0300

    ay 7aga

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
-import { Navbar } from "../Navbar/Navbar";
+import Navbar from "./Navbar";
 
 const Layout = () => {
   return (
     <>
-      <div className="container mx-auto px-4">
-        <Navbar />
+      <div className="min-h-screen flex flex-col w-full">
+        <Navbar
+          user={{ name: "Sarah", avatarUrl: "https://i.pravatar.cc/40" }}
+        />
+
+        <div className="container mx-auto">
+          <Outlet />
+        </div>
       </div>
-      <Outlet />
     </>
   );
 };
